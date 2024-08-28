9d4a3f8568c79047a1ecf1345b798f22b94e4df6
shychee <shychee96@gmail.com>
2024-08-28 11:46:43 +0800

    update lifrtimes

--- exercises/lifetimes/lifetimes2.rs
+++ exercises/lifetimes/lifetimes2.rs
@@ -6,8 +6,6 @@
 // Execute `rustlings hint lifetimes2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
@@ -18,10 +16,14 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
 
 fn main() {
     let string1 = String::from("long string is long");
+    // 1. Move the string2 declaration to make it live as long as string1 (how is result declared?)
+    let string2 = String::from("xyz");
     let result;
     {
-        let string2 = String::from("xyz");
+        // let string2 = String::from("xyz");
         result = longest(string1.as_str(), string2.as_str());
+        // 2. Move println! into the inner block
+        // println!("The longest string is '{}'", result);
     }
     println!("The longest string is '{}'", result);
 }
